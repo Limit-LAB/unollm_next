276ea32b43eabc9a074c15d5fe262c453fc1373e
KevinZønda <33132228+KevinZonda@users.noreply.github.com>
2023-11-22 02:23:43 +0000

    remove useless

--- relay/transformer.go
+++
@@ -1,9 +0,0 @@
-package relay
-
-import (
-	"github.com/sashabaranov/go-openai"
-	"limit.dev/unollm/model"
-)
-
-type GrpcTransformer func(any) (*model.LLMResponseSchema, error)
-type OpenAICompletionTransformer func(any) (openai.ChatCompletionResponse, error)
