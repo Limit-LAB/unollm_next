a572b7d267bb3c7bd39cbb27b23e4d55897682c0
KevinZønda <33132228+KevinZonda@users.noreply.github.com>
2023-11-30 21:58:31 +0000

    api

---
+++ singleInstance/api.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+rm -fr ./tmp
+mkdir -p ./tmp
+
+openapi-generator-cli generate -i ./UnoLLM.json -g go -o ./tmp --additional-properties=packageName=apimodel
+
+rm -fr ./model/apimodel
+mkdir -p ./model/apimodel
+mv -f ./tmp/model_*.go ./model/apimodel
+mv -f ./tmp/utils.go ./model/apimodel
+rm -rf ./tmp
+go fmt ./model/...
--- singleInstance/svc/keyStore/svc.go
+++ singleInstance/svc/keyStore/svc.go
@@ -3,62 +3,11 @@ package keyStore
 import (
 	"errors"
 	"github.com/gin-gonic/gin"
-	"go.limit.dev/unollm/httpHandler"
-	"go.limit.dev/unollm/singleInstance/model/dbmodel"
-	"go.limit.dev/unollm/singleInstance/shared"
-	"math/rand"
 )
 
 type KeyStoreSvc struct{}
 
 var ErrNoKey = errors.New("no key provided")
-
-func KeyTransformer(keyIn string, provider string) (httpHandler.KeyTransformerResult, error) {
-	var userKey dbmodel.UserDefinedKey
-	err := shared.GetDB().
-		Where("key = ?", keyIn).
-		First(&userKey).Error
-	if err != nil {
-		return httpHandler.KeyTransformerResult{}, err
-	}
-
-	var keyRelation []dbmodel.MapOriginRelation
-	err = shared.GetDB().
-		Where("map_id = ?", userKey.ID).
-		Find(&keyRelation).Error
-	if err != nil {
-		return httpHandler.KeyTransformerResult{}, err
-	}
-	if len(keyRelation) == 0 {
-		return httpHandler.KeyTransformerResult{}, ErrNoKey
-	}
-
-
-	var originKeyIds []uint
-	for _, relation := range keyRelation {
-		originKeyIds = append(originKeyIds, relation.OriginID)
-	}
-
-	var keys []dbmodel.OriginKey
-	err = shared.GetDB().
-		Where("id in ?", originKeyIds).
-		Where("provider = ?", provider).
-		Where("disabled = ?", false).
-		Find(&keys).Error
-	if err != nil {
-		return httpHandler.KeyTransformerResult{}, err
-	}
-	if len(keys) == 0 {
-		return httpHandler.KeyTransformerResult{}, ErrNoKey
-	}
-
-	key := keys[rand.Intn(len(keys))]
-
-	return httpHandler.KeyTransformerResult{
-		Key:      key.Key,
-		EndPoint: key.EndPoint,
-	}, nil
-}
 
 func (svc *KeyStoreSvc) RegisterRouter(g gin.IRouter, prefix string) {
 	_ = g.Group(prefix)
